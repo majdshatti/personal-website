996320f6b7dcc28ea8b9589f4457304015bceca2
majdshatti <majdshatti8@gmail.com>
2023-10-15 23:38:46 +0300

    :zap: feat: add skills data file

---
+++ src/data/skillsData.js
@@ -0,0 +1,82 @@
+export const skillsData = [
+  {
+    name: "HTML/CSS/JS",
+    categories: ["frontend"],
+  },
+  {
+    name: "PHP",
+    categories: ["backend"],
+  },
+  {
+    name: "Laravel",
+    categories: ["frontend", "backend"],
+  },
+  {
+    name: "Typescript",
+    categories: ["frontend", "backend"],
+  },
+  {
+    name: "Express",
+    categories: ["backend"],
+  },
+  {
+    name: "Nest.js",
+    categories: ["backend"],
+  },
+  {
+    name: "Redis",
+    categories: ["database", "caching"],
+  },
+  {
+    name: "Tailwind",
+    categories: ["frontend"],
+  },
+  {
+    name: "Vite",
+    categories: ["frontend"],
+  },
+  {
+    name: "React.js",
+    categories: ["frontend"],
+  },
+  {
+    name: "AWS (IAM/S3/EC2)",
+    categories: ["cloud"],
+  },
+  {
+    name: "Mysql",
+    categories: ["database"],
+  },
+  {
+    name: "MongoDB",
+    categories: ["database"],
+  },
+  {
+    name: "Typeorm",
+    categories: ["database"],
+  },
+  {
+    name: "Mongoose",
+    categories: ["database"],
+  },
+  {
+    name: "Socket.io",
+    categories: ["real-time"],
+  },
+  {
+    name: "Pusher",
+    categories: ["real-time"],
+  },
+  {
+    name: "Server Deployment",
+    categories: ["server", "deployment"],
+  },
+  {
+    name: "Linux",
+    categories: ["server", "deployment"],
+  },
+  {
+    name: "Nginx",
+    categories: ["server", "deployment"],
+  },
+];
