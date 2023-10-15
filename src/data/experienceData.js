f30bc08d57054a611db5487db04122f63d8d8f4c
majdshatti <majdshatti8@gmail.com>
2023-10-15 23:38:33 +0300

    :zap: feat: add experience data file

---
+++ src/data/experienceData.js
@@ -0,0 +1,31 @@
+export const experienceData = [
+  {
+    title: "FullStack Developer",
+    slug: "fullstack-developer-pancode",
+    fromDate: "12/2022",
+    toDate: "10/2023",
+    description: null,
+    keyPoints: [
+      "Played a key role in developing Laravel projects.",
+      "Actively participated in project planning and requirements gathering sessions. Throughout the development process, I collaborated with designers, front-end developers to ensure seamless integration and deliver exceptional user experiences.",
+      "Gained exposure to deploying and hosting applications, albeit in a limited capacity. I assisted with the deployment of landing websites, working alongside server administrators to ensure smooth transitions from development to production environments.",
+    ],
+    company: {
+      name: "Pancode",
+      url: "https://pan-code.com",
+    },
+  },
+  {
+    title: "Backend Developer",
+    slug: "backend-developer-gray-scale",
+    fromDate: "07/2021",
+    toDate: "12/2021",
+    description:
+      "During my three-month internship at GrayScale, I dedicated my time to intensive learning and skill development in the field of web development. While I did not participate in specific projects, this experience allowed me to focus on honing my technical skills and gaining a deeper understanding of key concepts in software engineering.",
+    keyPoints: [],
+    company: {
+      name: "GrayScale",
+      url: null,
+    },
+  },
+];
