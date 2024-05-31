ec7dc7f8279d8224bcf4ec8c8b28ee348d6f40a8
majdshatti <majdshatti8@gmail.com>
2024-05-31 20:28:04 +0400

    :zap: initialize tag manager

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "aos/dist/aos.css";
+import TagManager from "react-gtm-module";
+
+const tagManagerArgs = {
+  gtmId: import.meta.env.VITE_GTM_ID,
+};
+
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
